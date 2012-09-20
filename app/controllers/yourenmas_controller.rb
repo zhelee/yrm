f4acbe59f5a644f6c54edd899abb9f648bd6524b
lizhe <me@lizhe.name>
2012-09-20 22:54:46 +0800

    create yrm

--- app/controllers/yourenmas_controller.rb
+++ app/controllers/yourenmas_controller.rb
@@ -1,14 +1,27 @@
 class YourenmasController < ApplicationController
 
+  respond_to :json
+  skip_before_filter  :verify_authenticity_token
+
   def index
     if(params[:topic])
-      @yourenmas = Topic.find_by_name(params[:topic]).yourenma
+      @yourenmas = Topic.find_by_name(params[:topic]).yourenma.order("created_at DESC")
     else
       @yourenmas = Yourenma.order("created_at DESC").all
     end
-    respond_to do |format|
-      format.json
-    end
+
+    respond_with @yourenmas
+  end
+
+  def create
+    user_id = User.find_by_username(params[:user]).id
+    topic_id = Topic.find_by_name(params[:topic]).id
+    yourenma = Yourenma.create({
+      :user_id => user_id,
+      :topic_id => topic_id,
+      :description => params[:description]
+    })
+    respond_with yourenma
   end
 
 end
